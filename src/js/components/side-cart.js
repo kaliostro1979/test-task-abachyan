a56b68188a44ad8e80bbc76f7b7261b7186d11de
Misha <abachyanm@gmail.com>
2021-03-31 20:01:33 +0400

    body scroll disabled when side cart is open

--- src/js/components/side-cart.js
+++ src/js/components/side-cart.js
@@ -2,6 +2,7 @@ let sideCartBagButton = document.querySelectorAll('.shopping-card-icon')
 let sideCartCloseButton = document.querySelector('.side-cart__quantity__close')
 let sideCart = document.querySelector('.side-cart__main')
 let overlay = document.querySelector('.overlay')
+let body = document.querySelector('body')
 
 
 
@@ -9,16 +10,19 @@ for (let i = 0; i < sideCartBagButton.length; i++){
     sideCartBagButton[i].addEventListener('click', function (){
         sideCart.classList.add('open-cart')
         overlay.classList.add('overlay-open')
+        body.classList.add('covered')
     })
 }
 
 sideCartCloseButton.addEventListener('click', ()=>{
     sideCart.classList.remove('open-cart')
     overlay.classList.remove('overlay-open')
+    body.classList.remove('covered')
 })
 
 overlay.addEventListener('click', function (){
     sideCart.classList.remove('open-cart')
     overlay.classList.remove('overlay-open')
+    body.classList.remove('covered')
 })
 
